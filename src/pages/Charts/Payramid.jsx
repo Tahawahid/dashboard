c92dd34147c3318ed4d0c7e6e09b7e5f237f9f84
Tahawahid <muhammadtahawahid1@gmail.com>
2023-07-13 14:46:26 +0500

    Setup the pages required in the project

---
+++ src/pages/Charts/Payramid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Payramid = () => {
+  return (
+    <div>Payramid</div>
+  )
+}
+
+export default Payramid
---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+export {default as Calender} from './Calender';
+export {default as ColorPicker} from './ColorPicker';
+export {default as Editor} from './Editor';
+export {default as Employees} from './Employees';
+export {default as Kanban} from './Kanban';
+export {default as Order} from './Order';
+export {default as Ecommerce} from './Ecommerce';
+export {default as Customer} from './Customer';
+
+/* Exporting from Charts folder */  
+export {default as Area} from './charts/Area';
+export {default as Bar} from './charts/Bar';
+export {default as Financial} from './charts/Financial';
+export {default as Pie} from './charts/Pie';
+export {default as Payramid} from './charts/Payramid';
+export {default as Stacked} from './charts/Stacked';
+export {default as Line} from './charts/Line';
+export {default as ColorMapping} from './charts/ColorMapping';
