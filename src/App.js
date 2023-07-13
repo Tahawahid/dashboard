a3f2e624a4fdfcc6de90b3a94fce89c076e1bc52
Tahawahid <muhammadtahawahid1@gmail.com>
2023-07-13 15:08:10 +0500

    Remove the strings and call the respected components

--- src/App.js
+++ src/App.js
@@ -1,81 +1,105 @@
-import React, {useEffect} from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { FiSettings } from 'react-icons/fi'
-import {TooltipComponent} from '@syncfusion/ej2-react-popups'
-import './App.css'
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import {
+  Ecommerce,
+  Order,
+  Calender,
+  Employees,
+  Kanban,
+  Payramid,
+  Pie,
+  Line,
+  Stacked,
+  Bar,
+  Area,
+  ColorMapping,
+  Customer,
+  Editor,
+  ColorPicker,
+  Financial,
+} from "./pages";
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import "./App.css";
 
 const App = () => {
-  const activeMenu = true; 
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
-        <div className='flex relative dark:bg-main-dark-bg'>
-          <div className='fixed right-4 bottom-4' style={{zIndex : '1000'}}>
-            <TooltipComponent content='Settings' position='Top'>
-              <button 
-              type='button'
-              className='text-3xl p-3
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3
               hover:dropshadow-xl
               hover:bg-light-gray
-              text-white'
-              style={{background: 'blue', borderRadius:'50%'}}
+              text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
               >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className='w-72 fixed sidebar 
+            <div
+              className="w-72 fixed sidebar 
             dark:bg-secondary-dark-bg
-            bg-white'>
-              Sidebar
+            bg-white"
+            >
+              <Sidebar />
             </div>
           ) : (
-            <div className='w-0
-            dark:bg-secondary-dark-bg'>
-              Sidebar w-0
+            <div
+              className="w-0
+            dark:bg-secondary-dark-bg"
+            >
+              <Sidebar />
             </div>
           )}
-          <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'} `
-          }>
-            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-              Navbar
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            } `}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
             </div>
           </div>
           <div>
             <Routes>
               {/* Dashboard */}
-              <Route path='/' element='ECommerce'/>
-              <Route path='/ecommerce' element='ECommerce'/> 
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
 
               {/* Pages */}
-              <Route path='/order' element='Order' />
-              <Route path='/employees' element='Employees' />
-              <Route path='/customers' element='Customers' />
+              <Route path="/order" element={<Order />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customer />} />
 
               {/* Apps */}
-              <Route path='/kanban' element='Kanban' />
-              <Route path='/calender' element='Calender' />
-              <Route path='/editor' element='Editor' />
-              <Route path='/color-picker' element='ColorPicker' />
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/calender" element={<Calender />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
 
-               {/* Charts */}
-              <Route path='/line' element='Line' />
-              <Route path='/area' element='Area' />
-              <Route path='/bar' element='Bar' />
-              <Route path='/pie' element='Pie' />
-              <Route path='/financial' element='Financial' />
-              <Route path='/color-mapping' element='Color-Mapping' />
-              <Route path='/pyramid' element='Pyramid' />
-
-
+              {/* Charts */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Payramid />} />
             </Routes>
           </div>
         </div>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,18 @@
-export {default as Calender} from './Calender';
-export {default as ColorPicker} from './ColorPicker';
-export {default as Editor} from './Editor';
-export {default as Employees} from './Employees';
-export {default as Kanban} from './Kanban';
-export {default as Order} from './Order';
-export {default as Ecommerce} from './Ecommerce';
-export {default as Customer} from './Customer';
+export { default as Calender } from "./Calender";
+export { default as ColorPicker } from "./ColorPicker";
+export { default as Editor } from "./Editor";
+export { default as Employees } from "./Employees";
+export { default as Kanban } from "./Kanban";
+export { default as Order } from "./Order";
+export { default as Ecommerce } from "./Ecommerce";
+export { default as Customer } from "./Customer";
 
-/* Exporting from Charts folder */  
-export {default as Area} from './charts/Area';
-export {default as Bar} from './charts/Bar';
-export {default as Financial} from './charts/Financial';
-export {default as Pie} from './charts/Pie';
-export {default as Payramid} from './charts/Payramid';
-export {default as Stacked} from './charts/Stacked';
-export {default as Line} from './charts/Line';
-export {default as ColorMapping} from './charts/ColorMapping';
+/* Exporting from Charts folder */
+export { default as Area } from "./Charts/Area";
+export { default as Bar } from "./Charts/Bar";
+export { default as Financial } from "./Charts/Financial";
+export { default as Pie } from "./Charts/Pie";
+export { default as Payramid } from "./Charts/Payramid";
+export { default as Stacked } from "./Charts/Stacked";
+export { default as Line } from "./Charts/Line";
+export { default as ColorMapping } from "./Charts/ColorMapping";
